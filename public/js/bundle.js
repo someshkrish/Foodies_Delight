95ca1dab7b63f06b794c097a87a6aefc5af14ec5
somesh <somesh4653@gmail.com>
2021-06-23 07:55:58 +0530

    Version 1.0

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7024,7 +7024,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40479" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44083" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
